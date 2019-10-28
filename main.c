0e9d713c942408b9d374da08963f6120a8cb4d23
rleongPd5 <rleong00@stuy.edu>
2019-10-27 21:38:44 -0400

    Began testing for print_list and insert_front

--- main.c
+++ main.c
@@ -3,5 +3,22 @@
 #include "linkedlist.h"
 
 int main(){
+  // struct node  n0, n1, n2, n3, n4, n5, n6, n7, n8;
+  // struct node* p0, p1, p2, p3, p4, p5, p6, p7, p8;
+
+  struct node *p0 = NULL;
+
+  printf("Begin testing for linkedlist: \n");
+  printf("Printing empty list: \n");
+  print_list(p0);
+
+  printf("Adding numbers -256 through 256: \n");
+  int i;
+  for (i = -256; i < 257; i++){
+     p0 = insert_front(p0, i);
+  }
+  print_list(p0)
+
+
   return 0;
 }
